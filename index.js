1387a81b751d290db95383d35fdaea2bd6a56c61
danielkamweru <daniel.kamweru@student.moringaschool.com>
2025-09-04 14:57:05 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+let  employee = {
+    name:"Sam",
+    streetAddress:"11 Broadway",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+return{
+       "name": "Sam","streetAddress": "11 Broadway" };
+}
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee
+}
+function  deleteFromEmployeeByKey(employee, key){
+    return{}
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+return employee;
+} 
+
+
+
+
+
+         
